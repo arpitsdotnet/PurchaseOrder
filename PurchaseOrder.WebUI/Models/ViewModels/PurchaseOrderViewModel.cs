2996621e5d94475bf2e74cc8261c06b066f46f68
Arpit Shrivastava <shrivastava.arpit@hotmail.com>
2021-12-01 09:51:29 +0530

    PurchaseOrderViewModel has been added to for multiple models in View.

---
+++ PurchaseOrder.WebUI/Models/ViewModels/PurchaseOrderViewModel.cs
@@ -0,0 +1,18 @@
+﻿using PurchaseOrder.Services.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PurchaseOrder.WebUI.Models.ViewModels
+{
+    public class PurchaseOrderViewModel
+    {
+        public IEnumerable<PartyMasterModel> PartyMasters { get; set; }
+        public IEnumerable<ItemMasterModel> ItemMasters { get; set; }
+
+        public POMasterModel PurchaseOrder { get; set; }
+        public PODetailsModel PODetails { get; set; }
+        public List<PODetailsModel> PODetailsList { get; set; }
+    }
+}
